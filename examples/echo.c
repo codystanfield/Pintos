fb72cc1c15a977fc46286b86fa6133323345c176
TheBassist95 <jshowe95@gmail.com>
2015-04-11 17:13:18 -0500

    Echo example

--- examples/echo.c
+++ examples/echo.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <syscall.h>
+#include <unistd.h>
+#include "kernel/syscall.h"
 
 int
 main (int argc, char **argv)
 {
   int i;
 
-  for (i = 0; i < argc; i++)
-    printf ("%s ", argv[i]);
+  //for (i = 0; i < 2; i++){
+    printf("IMA FORKING MY PROCESS.\n");
+    exec("echo x");
+  //}
   printf ("\n");
 
   return EXIT_SUCCESS;
